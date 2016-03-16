4abe2610226ecf62950c664a57fe12d8860040f4
mz7116 <mz7116@mct165s16.cs.ship.edu>
2016-03-16 11:18:13 -0400

    Changed dataMethod() method (change 2)

--- Compiler440/src/parser/states/Rabe.java
+++ Compiler440/src/parser/states/Rabe.java
@@ -11,19 +11,12 @@ public class Rabe
 	
 	public int dataMethod()
 	{
-		int result = 0;
-
 		for (int x=0;x<name.length();x++)
-
 		{
-
-		result = name.charAt(x) + result;
-
+			data = name.charAt(x) + result;
 		}
-
-		data = result % name.length();
-
-		return result;
+		data = data % name.length();
+		return data;
 	}
 	
 	@Override
