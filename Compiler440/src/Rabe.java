6566283dc598c41dbabec280785d7c58510ee4ff
cr7723 <cr7723@cs.ship.edu>
2016-03-16 10:17:41 -0400

    Adding Rabe class (Part 1)

---
+++ Compiler440/src/Rabe.java
@@ -0,0 +1,21 @@
+public class Rabe
+{
+	int data;
+	String name;
+	
+	public void nameMethod()
+	{
+		
+	}
+	
+	public void dataMethod()
+	{
+		
+	}
+	
+	@Override
+	public String toString()
+	{
+		return name + data;
+	}
+}
