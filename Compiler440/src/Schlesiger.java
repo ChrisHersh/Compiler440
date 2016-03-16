26d52d90511bbadf8e92a9df6aceb53165281dde
schlesigerjess <schlesigerjess@gmail.com>
2016-03-16 10:24:24 -0400

    Member 2's work

--- Compiler440/src/Schlesiger.java
+++ Compiler440/src/Schlesiger.java
@@ -2,14 +2,19 @@
 public class Schlesiger {
 	int data;
 	String name;
-	
+
 	public void nameMethod() {
-		
+
 	}
-	public void dataMethod() {
-	
+	public int dataMethod() {
+		int result = 0;
+		for (int x=0;x<name.length();x++) {
+			result = name.charAt(x) + result;
+		}
+		data = result % name.length();
+		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name+data;
