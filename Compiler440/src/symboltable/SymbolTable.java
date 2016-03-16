a838ef749df6cbb3a7ffe1b700b1ed334d32a99a
IanKeefer <iantkeefer@aol.com>
2016-03-16 13:40:40 -0400

    Removed layer of CRUD operations. It created a unneeded layer of restriction.

--- Compiler440/src/symboltable/SymbolTable.java
+++ Compiler440/src/symboltable/SymbolTable.java
@@ -20,35 +20,6 @@ public class SymbolTable
 	{
 		table = new HashMap<String, ArrayList<Variable>>();
 	}
-
-    /**
-     * Add a variable list to the hash table.
-     * @param key
-     * @param list
-     */
-    public void addToTable(String key, ArrayList<Variable> list) 
-    {
-    	table.put(key, list);
-    }
-    
-    /**
-     * Remove a variable list from the hash table.
-     * @param key
-     */
-    public void removeFromTable(String key) 
-    {
-    	table.remove(key);
-    }
-    
-    /**
-     * Update a variable list in the hash table.
-     * @param key
-     * @param list
-     */
-    public void updateObjectInTable(String key, ArrayList<Variable> list) 
-    {
-    	table.replace(key, list);
-    }
     
     /**
      * @return singleton of SymbolTable
