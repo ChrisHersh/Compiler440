5c7f5de1b2639edb2702e44646bb695fe54583ca
Curtis Rabe <cr7723@cs.ship.edu>
2016-03-18 10:32:44 -0400

    Created primitive and object method vars to use for method parameters.

--- Compiler440/tests/symboltable/TestVariables.java
+++ Compiler440/tests/symboltable/TestVariables.java
@@ -8,10 +8,12 @@ import symboltable.variable.impl.InstanceObjectVariable;
 import symboltable.variable.impl.InstancePrimitiveVariable;
 import symboltable.variable.impl.LocalObjectVariable;
 import symboltable.variable.impl.LocalPrimitiveVariable;
+import symboltable.variable.impl.MethodObjectVariable;
+import symboltable.variable.impl.MethodPrimitiveVariable;
 
 /**
  * Tests the children of the abstract object Variable.
- * @author Ian Keefer
+ * @author Ian Keefer and Curtis Rabe
  */
 public class TestVariables {
 	
@@ -24,6 +26,10 @@ public class TestVariables {
 		LocalObjectVariable lov = new LocalObjectVariable("Name", "ClassName", "id", "MethodName", VariableScope.LOCAL, VariableType.OBJECT);
 		InstancePrimitiveVariable ipv = new InstancePrimitiveVariable("Name", "ClassName", VariableScope.INSTANCE, VariableType.BOOLEAN);
 		InstanceObjectVariable iov = new InstanceObjectVariable("Name", "ClassName", "id", VariableScope.INSTANCE, VariableType.OBJECT);
+		//added by Curtis Rabe
+		MethodPrimitiveVariable mpv = new MethodPrimitiveVariable("Name", "ClassName", "MethodName", VariableScope.METHOD, VariableType.INTEGER);
+		MethodObjectVariable mov = new MethodObjectVariable("Name", "ClassName", "id", "MethodName", VariableScope.METHOD, VariableType.OBJECT);
+
 		
 		assertEquals(lpv.getName(), "Name");
 		assertEquals(lpv.getClassName(), "ClassName");
@@ -52,6 +58,21 @@ public class TestVariables {
 		assertEquals(iov.getId(), "id");
 		assertEquals(iov.getScope(), VariableScope.INSTANCE);
 		assertEquals(iov.getType(), VariableType.OBJECT);
+		
+		//Tests and fragrance by Curtis Rabe
+		assertEquals(mpv.getName(), "Name");
+		assertEquals(mpv.getClassName(), "ClassName");
+		assertEquals(mpv.getMethodName(), "MethodName");
+		assertEquals(mpv.getId(), "");
+		assertEquals(mpv.getScope(), VariableScope.METHOD);
+		assertEquals(mpv.getType(), VariableType.INTEGER);
+		
+		assertEquals(mov.getName(), "Name");
+		assertEquals(mov.getClassName(), "ClassName");
+		assertEquals(mov.getMethodName(), "MethodName");
+		assertEquals(mov.getId(), "id");
+		assertEquals(mov.getScope(), VariableScope.METHOD);
+		assertEquals(mov.getType(), VariableType.OBJECT);
 	}
 
 }
