8a9c079b6660a89b4c3de0cbde5e759d6cfa571b
redja150 <redja150@gmail.com>
2016-03-18 11:22:27 -0400

    Added test that params are in order

--- Compiler440/tests/symboltable/TestMethods.java
+++ Compiler440/tests/symboltable/TestMethods.java
@@ -34,6 +34,22 @@ public class TestMethods
 		assertTrue(pm.getParams().contains(mpv2));
 
 	}
+	
+	/**
+	 * Tests that the parameters are in order ( Mike says its not needed (really, its superfluous) )
+	 */
+	@Test
+	public void testMethodParamOrder()
+	{
+		MethodPrimitiveVariable mpv1 = new MethodPrimitiveVariable("Name1", "ClassName", "MethodName", VariableScope.METHOD, VariableType.INTEGER);
+		MethodPrimitiveVariable mpv2 = new MethodPrimitiveVariable("Name2", "ClassName", "MethodName", VariableScope.METHOD, VariableType.BOOLEAN);
+		ArrayList<MethodPrimitiveVariable> params = new ArrayList <MethodPrimitiveVariable>();
+		params.add(mpv1);
+		params.add(mpv2);
+		PublicMethod pm = new PublicMethod("Name", "Class", VariableType.INTEGER, params);
+		assertTrue(pm.getParams().get(0).equals(mpv1));
+		assertTrue(pm.getParams().get(1).equals(mpv2));
+	}
 
 }
 
