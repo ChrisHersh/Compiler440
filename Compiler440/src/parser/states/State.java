c49f58d0acb58e81f3427a8d6e782ce1f2894c4c
ChrisHersh <ch5968@mct165l25>
2016-03-23 10:30:59 -0400

    Added method for default behavior for states

--- Compiler440/src/parser/states/State.java
+++ Compiler440/src/parser/states/State.java
@@ -2,12 +2,12 @@ package parser.states;
 
 import parser.Parser;
 
-public abstract class State 
+public abstract class State
 {
 	private State currentState;
 	private Parser currentParser;
 	
-	public State ()
+	public State()
 	{
 		this.currentParser = Parser.getInstance();
 //		this.currentState = currentParser.getStartState();//Sets the state to be the inital state in the state diagram
@@ -18,10 +18,21 @@ public abstract class State
 		currentState = newState;
 	}
 	
-	//Below here add all the methods for the different shift calls with our default logic
-	public void shiftEXP1 ()
+	/**
+	 * For default behavior for invalid states
+	 * This makes it easy to change the behavior for every shift
+	 * @author Chris Hersh
+	 * @throws ParserException 
+	 */
+	private void invalidState() throws ParserException
 	{
-		
+	    throw new ParserException();
+	}
+	
+	//Below here add all the methods for the different shift calls with our default logic
+	public void shiftEXP1 () throws ParserException
+	{
+		invalidState();
 	}
 	
 }
