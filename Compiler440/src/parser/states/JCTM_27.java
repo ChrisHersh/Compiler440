4936c38a3403da47a34c30cbd1224b4070724988
IanKeefer <iantkeefer@aol.com>
2016-03-24 12:57:20 -0400

    Added true, false, this, and id for JCTM_27

--- Compiler440/src/parser/states/JCTM_27.java
+++ Compiler440/src/parser/states/JCTM_27.java
@@ -1,5 +1,29 @@
 package parser.states;
 
+/**
+ * 
+ * @author Ian Keefer
+ */
 public class JCTM_27 extends State {
+	
+	public void shiftTrue()
+	{
+	    changeToState(new JCTM_15());
+	}
+
+	public void shiftFalse()
+	{
+	    changeToState(new JCTM_16());
+	}
+
+	public void shiftThis()
+	{
+	    changeToState(new JCTM_17());
+	}
+	
+	public void shiftId()
+	{
+	    changeToState(new JCTM_1());
+	}
 
 }
