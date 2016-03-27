169508fa481ed18d7f72c5f1fe403e241383bedc
Chad Nunemaker <can00336@gmail.com>
2016-03-27 15:41:34 -0400

    Add EXP6/7 transitions to JMCC_23

--- Compiler440/src/parser/states/JMCC_23.java
+++ Compiler440/src/parser/states/JMCC_23.java
@@ -2,5 +2,15 @@ package parser.states;
 
 public class JMCC_23 extends State
 {
-
+    @Override
+    public void shiftEXP6() throws ParserException
+    {
+        changeToState(new JMCC_24());
+    }
+    
+    @Override
+    public void shiftEXP7() throws ParserException
+    {
+        changeToState(new JMCC_29());
+    }
 }
