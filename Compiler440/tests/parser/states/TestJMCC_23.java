b0f1924ed4d08741c2903ab98ca22f39ac2e3d10
Chad Nunemaker <can00336@gmail.com>
2016-03-27 15:50:19 -0400

    Add some JUnit tests for JMCC_23
    Added Exp6/7 tests.

---
+++ Compiler440/tests/parser/states/TestJMCC_23.java
@@ -0,0 +1,79 @@
+package parser.states;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import parser.Parser;
+import tokenizer.Token;
+
+/**
+ * 
+ * @author Chad Nunemaker
+ * 
+ * In Ian and Chris's test style. 
+ *
+ */
+public class TestJMCC_23
+{
+    @Before
+    public void setUp()
+    {
+        Parser.resetParser();
+    }
+    
+    @Test
+    public void testShiftEXP6() throws ParserException
+    {
+        Parser p = Parser.getInstance();
+        State s = new JMCC_23();
+        
+        Token inpToken = new Token("exp6", "EXP6", 42);
+        
+        p.getInputStack().push(inpToken);
+        
+        assertFalse(p.getInputStack().empty());
+        assertEquals(p.getInputStack().peek(), inpToken);
+        assertTrue(p.getHoldStack().empty());
+        assertTrue(p.getStateStack().empty());
+        
+        s.shiftEXP7();
+        
+        assertTrue(p.getInputStack().empty());
+        assertFalse(p.getHoldStack().empty());
+        assertFalse(p.getStateStack().empty());
+        
+        assertEquals(p.getHoldStack().peek(), inpToken);
+        assertEquals(p.getStateStack().peek(), s);
+        
+        assertTrue(p.getCurrentState() instanceof JMCC_24);
+    }
+    
+    @Test
+    public void testShiftEXP7() throws ParserException
+    {
+        Parser p = Parser.getInstance();
+        State s = new JMCC_23();
+        
+        Token inpToken = new Token("exp7", "EXP7", 42);
+        
+        p.getInputStack().push(inpToken);
+        
+        assertFalse(p.getInputStack().empty());
+        assertEquals(p.getInputStack().peek(), inpToken);
+        assertTrue(p.getHoldStack().empty());
+        assertTrue(p.getStateStack().empty());
+        
+        s.shiftEXP7();
+        
+        assertTrue(p.getInputStack().empty());
+        assertFalse(p.getHoldStack().empty());
+        assertFalse(p.getStateStack().empty());
+        
+        assertEquals(p.getHoldStack().peek(), inpToken);
+        assertEquals(p.getStateStack().peek(), s);
+        
+        assertTrue(p.getCurrentState() instanceof JMCC_29);
+    }
+}
