b80595c7f7e72804172ed3b9933ec822579b6c4f
IanKeefer <ik5218@ship.edu>
2016-03-28 10:01:29 -0400

    Changed a package name.

--- Compiler440/tests/symboltable/TestMethods.java
+++ Compiler440/tests/symboltable/TestMethods.java
@@ -1,4 +1,4 @@
-package tests.symboltable;
+package symboltable;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
