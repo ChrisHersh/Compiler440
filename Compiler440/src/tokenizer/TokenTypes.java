4de195df5f382e6a053cc701c4c7584db1c42d06
ChrisHersh <Chris@ChrisHersh.com>
2016-03-28 11:23:39 -0400

    Added token types enum

---
+++ Compiler440/src/tokenizer/TokenTypes.java
@@ -0,0 +1,101 @@
+package tokenizer;
+
+/**
+ * Enum for the various types of tokens
+ * @author Chris Hersh
+ *
+ */
+public enum TokenTypes
+{
+    //Terminals
+    //Single comment
+    SLComment,
+    
+    // Parenthesis and braces
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    LPara,
+    RPara,
+    
+    //keywords
+    Int,
+    Boolean,
+    If,
+    While,
+    Void,
+    Main,
+    Class,
+    Static,
+    Extends,
+    New,
+    Public,
+    Else,
+    Return,
+    SystemOutPrintln,
+    Length,
+    This,
+    True,
+    False,
+    
+    //Symbols
+    Comma,
+    And,
+    Or,
+    Equals,
+    GtEquals,
+    LtEquals,
+    NotEquals,
+    SemiColon,
+    Plus, 
+    Minus,
+    Mult,
+    Assignment,
+    Period,
+    Gt,
+    Lt,
+    Not,
+    
+    //Integer
+    IntNum,
+    
+    //ID
+    Id,
+    
+    //Junk
+    Junk,
+    
+    //Non-Terminals
+    PROG,
+    CLASS_DECL_L,
+    MAIN_CLASS,
+    MAIN_METHOD,
+    CLASS_DECL,
+    M_METH_BODY,
+    VAR_DECL_L,
+    METH_DECL_L,
+    VAR_DECL,
+    METHOD_DECL,
+    METH_BODY,
+    FORMAL_L,
+    FORMAL_R,
+    TYPE,
+    STMT,
+    STMT_P,
+    EXP1,
+    EXP2,
+    EXP3,
+    EXP4,
+    EXP5,
+    EXP6,
+    EXP7,
+    OP1,
+    OP2,
+    OP3,
+    OP4,
+    OP5,
+    OP6,
+    EXP_L,
+    EXP_R,
+}
