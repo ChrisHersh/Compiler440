af94a779a1a1b0c80d64f41cececbdaa84e5a5f4
jl9857 <jl9857@mct165l09>
2016-03-30 10:04:25 -0400

    Added override of invalidState() method in CRM_1 to handle empty string reduce.

--- Compiler440/src/parser/states/CRM_1.java
+++ Compiler440/src/parser/states/CRM_1.java
@@ -1,5 +1,7 @@
 package parser.states;
 
+import tokenizer.TokenTypes;
+
 public class CRM_1 extends State
 {
 	/**
@@ -9,5 +11,11 @@ public class CRM_1 extends State
 	public void shiftVAR_DECL_L() throws ParserException
 	{
 		changeToState(new CRM_2());
+	}
+	
+	@Override
+	protected void invalidState() throws ParserException
+	{
+		reduceToState(this, TokenTypes.VAR_DECL_L);
 	}
 }
