6bc67506e405cc409914535f59d084c2049aa04b
Michael Zimmerman <mz7116@cs.ship.edu>
2016-03-30 22:30:13 -0400

    Made MDJ_9, _15, _16, and corresponding test. Had to make many other (blank) states to get test to work

---
+++ Compiler440/src/parser/states/MDJ_16.java
@@ -0,0 +1,21 @@
+package parser.states;
+
+import tokenizer.TokenTypes;
+
+/**
+ * changes to state MDJ_7
+ * @author Mike
+ *
+ */
+public class MDJ_16 extends State
+{
+	/**
+	 * reduces to MDJ_7 and generates the token MAIN_METHOD 
+	 */
+	@Override
+	protected void invalidState() throws ParserException
+	{
+		reduceToState(new MDJ_7(), TokenTypes.MAIN_METHOD);
+	}
+
+}
