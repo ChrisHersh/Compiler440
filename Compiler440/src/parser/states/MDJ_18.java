3de2fce6e660778a86a125c638f5176c51abdeca
Jessica <Jessica@Jessica-PC>
2016-04-02 14:43:00 -0400

    Added my state changes tasks Class_Dec(L) - 1 and Class_Dec(L) - 2

--- Compiler440/src/parser/states/MDJ_18.java
+++ Compiler440/src/parser/states/MDJ_18.java
@@ -1,5 +1,17 @@
 package parser.states;
-
-public class MDJ_18 extends State {
-
+/**
+ * 
+ * @author Jessica Schlesiger
+ * MDJ_18 state. Ensure this state can properly shift to MDJ_20 on "{" token
+ */
+public class MDJ_18 extends State
+{
+	/**
+	 * Changes to state MDJ_20 on "{" token
+	 */
+	@Override
+	public void shiftLeftBrace() throws ParserException
+	{
+		changeToState(new MDJ_20());
+	}
 }
--- Compiler440/src/parser/states/MDJ_3.java
+++ Compiler440/src/parser/states/MDJ_3.java
@@ -1,5 +1,17 @@
 package parser.states;
-
-public class MDJ_3 extends State{
-
+/**
+ * 
+ * @author Jessica Schlesiger
+ * MDJ_3 state. Ensure this state can properly shift to MDJ_17 on "class" token
+ */
+public class MDJ_3 extends State
+{
+	/**
+	 * Changes to state MDJ_17 on "class" token
+	 */
+	@Override
+	public void shiftClass() throws ParserException
+	{
+		changeToState(new MDJ_17());
+	}
 }
