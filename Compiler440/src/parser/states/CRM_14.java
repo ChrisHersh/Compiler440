d2acaaecd45e0c332f2eb2178e6cb9dbbc3c4b57
jl9857 <jl9857@mct164l02>
2016-04-05 12:27:55 -0400

    Completed states CRM_14 and CRM_15, still need to write tests

--- Compiler440/src/parser/states/CRM_14.java
+++ Compiler440/src/parser/states/CRM_14.java
@@ -1,6 +1,30 @@
 package parser.states;
+/**
+ * @author Jason LoBianco
+ */
+import tokenizer.Token;
+import tokenizer.TokenTypes;
 
 public class CRM_14 extends State
 {
-
+	/**
+	 * Changes to state CRM_15
+	 * @author Jason LoBianco
+	 * @throws ParserException
+	 */
+	public void shiftVAR_DECL_L() throws ParserException
+	{
+		changeToState(new CRM_15());
+	}
+	
+	/**
+	 * reduces to the current state and generates the token VAR_DECL_L
+	 * @author Jason LoBianco
+	 * @throws ParserException
+	 */
+	@Override
+	protected void invalidState() throws ParserException
+	{
+		currentParser.pushInputStack(new Token(TokenTypes.VAR_DECL_L.name(), TokenTypes.VAR_DECL_L.toString(), currentParser.peekInputStack().getLineNumber()));
+	}
 }
