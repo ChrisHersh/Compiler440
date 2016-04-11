908f1a8b6d8e0e989a7c1087724d5bc9318fbebe
ChrisHersh <Chris@ChrisHersh.com>
2016-04-11 10:14:50 -0400

    Fixed test after epsilon

--- Compiler440/tests/tokenizer/TestTokenTypes.java
+++ Compiler440/tests/tokenizer/TestTokenTypes.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 
 public class TestTokenTypes {
 	
-	private final int TOKEN_AMOUNT = 76; // 45 Terminals, 31 Non-terminals
+    private final int terminals = 45;
+    private final int non_terminals = 31;
+    private final int epsilon = 1;
+	private final int TOKEN_AMOUNT = terminals + non_terminals + epsilon; // 45 Terminals, 31 Non-terminals, 1 epsilon
 	
 	@SuppressWarnings("unused") // Ignoring the warning for non using type. No other way to test.
 	@Test
