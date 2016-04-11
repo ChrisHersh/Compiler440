598a0b38834291fc0d40a30a27456d2fe987e05d
ma7488 <ma7488@mct165l18>
2016-04-11 11:06:20 -0400

    fix state & test 32

--- Compiler440/src/parser/states/CILS_32.java
+++ Compiler440/src/parser/states/CILS_32.java
@@ -3,7 +3,7 @@ package parser.states;
 import tokenizer.TokenTypes;
 
 /**
- *This class is CILS_32 which represents this: STMT -> if ( EXP1 ) STMT
+ *This class is CILS_32 which represents this: STMT -> While ( EXP1 ) STMT
  * 
  * @author Mohammed
  */
@@ -13,7 +13,7 @@ public class CILS_32 extends State
 	 * because we do not have no more inputs, reduce to CILS_5
 	 */
 	@Override
-	public void invalidState() throws ParserException
+	protected void invalidState() throws ParserException
 	{
 		reduceToState(new CILS_5(), TokenTypes.STMT);
 	}
