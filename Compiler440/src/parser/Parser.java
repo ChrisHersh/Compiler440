429de2ac55043ceb5ac712c77aa638a390d6361e
DBreitigan <danthemanb3@gmail.com>
2016-04-11 10:08:47 -0400

    Fixed bug in Parser

--- Compiler440/src/parser/Parser.java
+++ Compiler440/src/parser/Parser.java
@@ -424,6 +424,8 @@ public class Parser
         case "EXP_R":
             getCurrentState().shiftEXP_R();
             break;  
+        default:
+            getCurrentState().shiftInvalidState();
         }
     }
 }
