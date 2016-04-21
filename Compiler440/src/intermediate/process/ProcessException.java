33c6e0759f183ce97b9e47b9f2e1aa07636a24e1
redja150 <redja150@gmail.com>
2016-04-21 14:14:10 -0400

    Worked on pass3 for STMT and broke token

---
+++ Compiler440/src/intermediate/process/ProcessException.java
@@ -0,0 +1,10 @@
+package intermediate.process;
+
+public class ProcessException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- Compiler440/src/intermediate/process/ProcessSTMT_P.java
+++ Compiler440/src/intermediate/process/ProcessSTMT_P.java
@@ -1,6 +1,7 @@
 package intermediate.process;
 
 import tokenizer.Token;
+import tokenizer.TokenTypes;
 
 public class ProcessSTMT_P {
 	public static void processPass1(Token subject) {
@@ -13,8 +14,32 @@ public class ProcessSTMT_P {
 		
 	}
 
-	public static void processPass3(Token subject) {
-		// TODO Auto-generated method stub
+	public static void processPass3(Token subject) throws ProcessException
+	{
+		
+		if(subject.getChildren().get(0).getTokenName() == TokenTypes.STMT_P.name())
+		{
+			ProcessSTMT_P.processPass3(subject.getChildren().get(0));
+			if(subject.getChildren().get(1).getTokenName() == TokenTypes.STMT.name())
+			{
+				ProcessSTMT_P.processPass3(subject.getChildren().get(1));
+			}
+			else 
+			{
+				throw new ProcessException();
+			}
+		}
+		else{
+			if(subject.getChildren().get(1).getTokenName() == TokenTypes.STMT.name())
+			{
+				ProcessSTMT_P.processPass3(subject.getChildren().get(1));
+			}
+			else 
+			{
+				throw new ProcessException();
+			}	
+		}
+		// TODO
 		
 	}
 }
