d3848e0dbcb0ad761b7efb372b6a868db3165bc7
Curtis Rabe <cr7723@cs.ship.edu>
2016-04-24 22:50:55 -0400

    Added prelim base for STMT

--- Compiler440/src/intermediate/process/ProcessSTMT.java
+++ Compiler440/src/intermediate/process/ProcessSTMT.java
@@ -1,10 +1,34 @@
 package intermediate.process;
 
 import tokenizer.Token;
+import tokenizer.TokenTypes;
 
-public class ProcessSTMT {
-	public static void processPass1(Token subject) {
-		// TODO Auto-generated method stub
+/**
+ * @author Curtis Rabe
+ * 
+ * Process class for STMT (the big one)
+ */
+public class ProcessSTMT
+{
+	/**
+	 * Using DFS, go through the possible STMT variations to process the STMT token
+	 * 
+	 * @param subject the incoming token
+	 */
+	public static void processPass1(Token subject)
+	{
+		//switch on the first child.getName...
+		switch(subject.getChildren().get(0).getTokenName())
+		{
+		//TODO - finish the stuff
+			case "LBrace":
+				if(subject.getChildren().get(1).getTokenName().equals(TokenTypes.STMT_P.name()))
+				{
+					ProcessSTMT_P.processPass1(subject.getChildren().get(1));
+				}
+			default:
+				break;
+		}
 		
 	}
 
