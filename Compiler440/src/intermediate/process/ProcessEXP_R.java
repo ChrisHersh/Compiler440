33842b2e53cdf2105c179c12ec0a0403edd5b109
Curtis Rabe <cr7723@cs.ship.edu>
2016-04-24 20:25:30 -0400

    Coded and documented my understanding of Pass1 for EXP_R. Currently thinking through testing...

--- Compiler440/src/intermediate/process/ProcessEXP_R.java
+++ Compiler440/src/intermediate/process/ProcessEXP_R.java
@@ -2,10 +2,35 @@ package intermediate.process;
 
 import tokenizer.Token;
 
-public class ProcessEXP_R {
-	public static void processPass1(Token subject) {
-		// TODO Auto-generated method stub
-		
+/**
+ * @author Curtis Rabe, Jared Good, Mike Zimmerman
+ * 
+ * The 3-Pass Process class for EXP_R
+ *
+ */
+public class ProcessEXP_R
+{
+	
+	/**
+	 * @author Curtis Rabe
+	 * Using DFS, processes the first child of EXP_R, but that is a comma, and
+	 * I do not believe we are processing terminals. Then processes the second child
+	 * (of 2 total children) which is EXP1. Nothing is added to the symboltable at this step,
+	 * because the program would not yet have come across an addable item.
+	 * 
+	 * Note, once EXP1 traverses down to EXP7, the ids will need to be added, but currently
+	 * EXP_R has no access to ids (classes, methods, or variables)
+	 * 
+	 * @param subject - the token input into the process
+	 */
+	public static void processPass1(Token subject)
+	{
+		if(subject.getChildren().get(0) != null)
+		{
+			//processing a terminal?
+			//ProcessComma.processPass1(subject.getChildren().get(0));
+			ProcessEXP1.processPass1(subject.getChildren().get(1));
+		}
 	}
 
 	public static void processPass2(Token subject) {
