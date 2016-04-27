6ade15ad4138010a466d6d902d92e13a97eed94c
thepure12 <tr3897@cs.ship.edu>
2016-04-27 13:39:25 -0400

    Created the tests for CRM_19-22 and got them green.

---
+++ Compiler440/tests/parser/states/CRM/TestCRM_21.java
@@ -0,0 +1,83 @@
+package parser.states.CRM;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import parser.Parser;
+import parser.states.ParserException;
+import parser.states.State;
+import tokenizer.Token;
+
+/**
+ * Tests for CRM_21
+ * @author TJ Renninger
+ */
+public class TestCRM_21
+{
+	private Parser p;
+	private State s;
+	private int i;
+	private final String[] toks = {"Id"};
+	private final Object[] states = {CRM_22.class};
+	
+	/**
+	 * Sets up everything for the tests.
+	 */
+	@Before
+    public void setUp()
+    {
+        Parser.resetParser();
+        p = Parser.getInstance();
+        s = new CRM_21();
+    }
+	
+	/**
+	 * Test that shiftInt() on while in CRM_21 goes to the correct state
+	 * @throws ParserException
+	 */
+	@Test
+	public void shiftId() throws ParserException
+	{
+		i = 0;
+	    Token token = getToken();
+	    checkStacksBeforeShift(token);
+	    s.shiftId();
+	    checkStacksAfterShift(token);
+	    assertTrue(p.getCurrentState().getClass() == states[i]);
+	}
+	
+	/**
+	 * @return returns the correct token for each test
+	 */
+	private Token getToken()
+	{
+		return new Token(toks[i], toks[i], 25);
+	}
+	
+	/**
+	 * Checks all the stacks before a shift
+	 * @param token That is being checked
+	 */
+	public void checkStacksBeforeShift(Token token)
+	{
+		p.getInputStack().push(token);
+	    assertFalse(p.getInputStack().empty());
+	    assertEquals(p.getInputStack().peek(), token);
+	    assertTrue(p.getHoldStack().empty());
+	    assertTrue(p.getStateStack().empty());
+	}
+	
+	/**
+	 * Checks all the stacks after a shift
+	 * @param token That is being checked
+	 */
+	public void checkStacksAfterShift(Token token)
+	{
+		assertTrue(p.getInputStack().empty());
+	    assertFalse(p.getHoldStack().empty());
+	    assertFalse(p.getStateStack().empty());
+	    assertEquals(p.getHoldStack().peek(), token);
+	    assertEquals(p.getStateStack().peek(), s);
+	}
+}
