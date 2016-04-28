f4ac063edeabf65f2075b8ab2a863f777fbed56e
IanKeefer <ik5218@ship.edu>
2016-04-27 20:02:24 -0400

    Added comments to JCTM_23

--- Compiler440/src/parser/states/JCTM/JCTM_23.java
+++ Compiler440/src/parser/states/JCTM/JCTM_23.java
@@ -5,12 +5,13 @@ import parser.states.State;
 import tokenizer.TokenTypes;
 
 /**
- * 
+ * JCTM_23 State
  * @author Ian Keefer
  *
  */
 public class JCTM_23 extends State {
 	
+	// Reduce state to EXP7
     @Override
     public void invalidState() throws ParserException
     {
